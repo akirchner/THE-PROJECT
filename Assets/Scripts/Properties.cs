86f2b0ff063fdadf9d265dc185c2c2d811180523
Thomas <thetardis1968@gmail.com>
2018-05-30 16:26:43 -0500

    removed unused method and added some stuff for sprite changing

--- Assets/Scripts/Properties.cs
+++ Assets/Scripts/Properties.cs
@@ -9,7 +9,7 @@ public class Properties : MonoBehaviour
 
     public string type;
     public float size;
-    public bool isMobile;
+    public string movementType;
     bool click = false;
 
     // Use this for initialization
@@ -17,20 +17,7 @@ public class Properties : MonoBehaviour
     {
       
         setSize(size);
-
-        switch (type.ToUpper())
-        {
-            case "GRAVITON":
-			GameObject.Find("Beam").GetComponent<Beam>().setGravitonCount(GameObject.Find("Beam").GetComponent<Beam>().gravitonCount + 1);
-                break;
-            case "ELECTRON":
-			GameObject.Find("Beam").GetComponent<Beam>().setElectronCount(GameObject.Find("Beam").GetComponent<Beam>().electronCount + 1);
-                break;
-            default:
-                Debug.Log("Invalid particle type! Type: " + type);
-                break;
-        }
-
+        setSprite();
     }
 
     // Update is called once per frame
@@ -39,49 +26,20 @@ public class Properties : MonoBehaviour
 
     }
 
-    private void OnGUI()
-    {
-        Vector2 r = new Vector2();
-        Camera c = Camera.main;
-        Event e = Event.current;
-        Vector2 mousePos = new Vector2();
-
-        Vector2 current = new Vector2(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y);
-
-        //Get the mouse position from Event.
-        //Note that the y position from Event is inverted.
-        mousePos.x = e.mousePosition.x;
-        mousePos.y = c.pixelHeight - e.mousePosition.y;
-
-        r = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y));
-
-
-        if (click)
-        {
-            GetComponent<Transform>().position = r;
-        }
-    }
-
-
-
     void setSize(float newSize)
     {
         GetComponent<Transform>().localScale = new Vector3(newSize, newSize, newSize);
     }
 
-    Color setColor(string type)
+    public void setSprite()
     {
-        switch (type)
+        try {
+            this.GetComponent<SpriteRenderer>().sprite = (Resources.Load(type.ToUpper() + "_" + movementType.ToUpper(), typeof(Sprite)) as Sprite);
+        }
+        catch(UnassignedReferenceException e)
         {
-            case "graviton":
-                return Color.green;
-            case "electron":
-                return Color.yellow;
-            case "fluxion":
-                return Color.magenta;
 
         }
-        return Color.gray;
     }
 
 }
