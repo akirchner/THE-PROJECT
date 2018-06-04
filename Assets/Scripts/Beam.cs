98518ac9bad665c2d40198bc0462429219713373
Thomas <thetardis1968@gmail.com>
2018-06-04 12:57:42 -0500

    time based beam

--- Assets/Scripts/Beam.cs
+++ Assets/Scripts/Beam.cs
@@ -6,6 +6,8 @@ public class Beam : MonoBehaviour
 {
 
     public Rigidbody2D particle;
+    private System.Diagnostics.Stopwatch timer;
+    private float initialMillis;
     public int angle;
     public int charge = 1;
     int counter = 0;
@@ -21,18 +23,20 @@ public class Beam : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        timer = new System.Diagnostics.Stopwatch();
+        timer.Start();
+        initialMillis = timer.ElapsedMilliseconds;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (counter == 5)
+        if(timer.ElapsedMilliseconds - initialMillis >= 120)
         {
             spawn(particle, angle);
-            counter = 0;
+            initialMillis = timer.ElapsedMilliseconds;
         }
-        counter++;
+
     }
 
 
