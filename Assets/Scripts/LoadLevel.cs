1b5d8d5cb2c82ac3bc81c23b84593b372988c0fe
Adrian <awkirchner@gmail.com>
2018-06-05 13:07:50 -0500

    I hate strings and chars

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -47,7 +47,7 @@ public class LoadLevel : MonoBehaviour {
                     currentObject = Instantiate(beam, new Vector3((float)levelData[i][1], (float)levelData[i][2]), Quaternion.Euler(0, 0, (float)levelData[i][3]));
                     beamProperties = new List<bool>();
                     foreach (char j in levelData[i][4].ToString()) {
-                        beamProperties.Add(j == '1' ? true : false);
+                        beamProperties.Add(j.Equals('1') ? true : false);
                     }
                     currentObject.GetComponent<Beam>().setProperites(beamProperties[0], beamProperties[1], beamProperties[2], beamProperties[3]);
                     break;
