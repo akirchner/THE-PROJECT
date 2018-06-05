7133075b60c55c6c1fe743a5255c9b36fcf8f3aa
Emilio Alvarez <e.l.alvarez@icloud.com>
2018-06-05 15:58:31 -0500

    Multi Goals

--- Assets/Scripts/Finalization.cs
+++ Assets/Scripts/Finalization.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Timers;
+            
+public class Finalization : MonoBehaviour
+{
 
-
-public class Finalization : MonoBehaviour {
-    
     public int targetCount;
     public Transform ProgressBar;
     int mParticleCount, mDecrementDelay;
-    bool end;
+    bool mFull;
+    List<bool> mEnd;
     Vector3 mStep;
     GameObject[] mGoals;
 
@@ -20,38 +21,55 @@ public class Finalization : MonoBehaviour {
 	void Start () {
         mDecrementDelay = 35;
         mStep = new Vector3((18f / targetCount), 0, 0);
-        end = false;
-	}
+        mFull = false;
+        mEnd = new List<bool>();
+        mGoals = GameObject.FindGameObjectsWithTag("Goal");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        mGoals = GameObject.FindGameObjectsWithTag("Respawn");
 
         foreach (GameObject i in mGoals) {
-            //i.GetComponents.
+            mEnd.Add(i.GetComponent<Finalization>().isFull());
         }
 
-        end = (mParticleCount >= targetCount) ? true : false;
-
-        if (end) Initiate.Fade("End", Color.black, 4f);
-
-        if (mDecrementDelay <= 0 && mParticleCount != 0) {
-            mParticleCount--;
-            ProgressBar.localScale -= mStep;
-            mDecrementDelay = 2 * 50/targetCount;
+        if (mEnd.TrueForAll(x => x)) {
+            Initiate.Fade("End", Color.black, 4f);
+        } else {
+            mEnd.Clear();
         }
+
+        updateCount(false);
         mDecrementDelay--;
-	}
+       	}
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Particle"))
         {
             Destroy(col.gameObject);
+            updateCount(true);
+        }
+    }
+
+    public bool isFull() {
+        return mFull;
+    }
+
+    void updateCount(bool increase) {
+        if (increase && mParticleCount <= targetCount)
+        {
             mParticleCount++;
             ProgressBar.localScale += mStep;
             mDecrementDelay = 35;
         }
+        else if (mDecrementDelay <= 0 && mParticleCount != 0)
+        {
+            mParticleCount--;
+            ProgressBar.localScale -= mStep;
+            mDecrementDelay = 2;
+        }
+        mFull = (mParticleCount >= targetCount) ? true : false;
     }
 
 }
