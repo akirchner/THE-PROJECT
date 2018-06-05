733fb01076894385334201d1ad68c5a3a865c344
Adrian <awkirchner@gmail.com>
2018-06-05 15:34:51 -0500

    updating references

--- Assets/Scripts/GameProperties.cs
+++ Assets/Scripts/GameProperties.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameProperties {
-    public static string levelFilename = "Levels/Grav2123.txt";
+    public static string levelFilename = "Assets/Levels/Grav2123.txt";
 }
