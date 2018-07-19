405028570b53f210fef322ac48e6b3e8c463b3bd
OcarinaOfTardis <thetardis1968@gmail.com>
2018-07-19 15:20:40 -0500

    Wormholes almost done

--- Assets/Scripts/Wormhole.cs
+++ Assets/Scripts/Wormhole.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wormhole : MonoBehaviour {
-    public int id;
+    public int id, lastID;
+    private Color color;
 
 	// Use this for initialization
 	void Start () {
-		
+        setColor();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(id != lastID)
+        {
+            setColor();
+        }
+
+        this.gameObject.transform.Rotate(new Vector3(0, 0, -1), Space.Self);
+        lastID = id;
 	}
+
+    private void setColor()
+    {
+        switch (id)
+        {
+            case 0:
+                color = Color.red;
+                break;
+            case 1:
+                color = Color.blue;
+                break;
+            case 2:
+                color = Color.green;
+                break;
+            case 3:
+                color = Color.magenta;
+                break;
+            case 4:
+                color = Color.yellow;
+                break;
+            case 5:
+                color = Color.cyan;
+                break;
+            default:
+                Debug.Log("What is my identity?");
+                break;
+        }
+
+        GetComponent<SpriteRenderer>().color = color;
+    }
 }
