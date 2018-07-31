104d1d0c4735567cef5a2c091f6cd2d52fd4899e
Zackamo <zackmj99@gmail.com>
2018-07-30 22:25:15 -0500

    added other panels to editor

---
+++ Assets/Scripts/Editor Scripts/NextPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPanel : MonoBehaviour {
+
+	public Transform thisPanel, nextPanel;
+
+	public void click(){
+		thisPanel.GetComponent<ClosePannel>().Close ();
+		nextPanel.GetComponent<ClosePannel>().Close ();
+	}
+}
