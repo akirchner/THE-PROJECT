b6940a41adb44bf13b8d5c5ff9e97e47de220ed9
OcarinaOfTardis <thetardis1968@gmail.com>
2018-08-04 10:43:10 -0500

    removed the mass, charge and fluxcapacity lists, saving time in particle

--- Assets/Scripts/Gameplay/Particle.cs
+++ Assets/Scripts/Gameplay/Particle.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
-    List<float> gravDistanceX, gravDistanceY, elecDistanceX, elecDistanceY, fluxDistanceX, fluxDistanceY, mMass, mCharge, mFluxCapacity;
+    List<float> gravDistanceX, gravDistanceY, elecDistanceX, elecDistanceY, fluxDistanceX, fluxDistanceY;
     private List<bool> mProperties;
     List<GameObject> mActiveForces;
     GameObject[] dragableF, staticF, dynamicF;
     float currentX, currentY;
     Vector2 gravForce, elecForce, fluxForce, resultant;
-    public float gravityConstant = 1;
-    public float electricConstant = 1;
-    public float fluxConstant = 1;
     public Rigidbody2D rb;
 
     // Use this for initialization
@@ -25,9 +22,6 @@ public class Particle : MonoBehaviour
         elecDistanceY = new List<float>();
         fluxDistanceX = new List<float>();
         fluxDistanceY = new List<float>();
-        mMass = new List<float>();
-        mCharge = new List<float>();
-        mFluxCapacity = new List<float>();
         mActiveForces = new List<GameObject>();
         gravForce = new Vector2();
         elecForce = new Vector2();
@@ -51,17 +45,14 @@ public class Particle : MonoBehaviour
                     case ForceType.Graviton:
                         gravDistanceX.Add((float)i.transform.position.x);
                         gravDistanceY.Add((float)i.transform.position.y);
-                        mMass.Add((float)i.GetComponent<Properties>().size);
                         break;
                     case ForceType.Electron:
                         elecDistanceX.Add((float)i.transform.position.x);
                         elecDistanceY.Add((float)i.transform.position.y);
-                        mCharge.Add((float)i.GetComponent<Properties>().size);
                         break;
                     case ForceType.Fluxion:
                         fluxDistanceX.Add((float)i.transform.position.x);
                         fluxDistanceY.Add((float)i.transform.position.y);
-                        mFluxCapacity.Add((float)i.GetComponent<Properties>().size);
                         break;
                     default:
                         break;
@@ -69,22 +60,19 @@ public class Particle : MonoBehaviour
             }
             catch(System.Exception)
             {
-                switch (i.GetComponent<DynamicProperties>().type)
+                switch (i.GetComponent<DynamicProperties>().production)
                 {
                     case ForceType.Graviton:
                         gravDistanceX.Add((float)i.transform.position.x);
                         gravDistanceY.Add((float)i.transform.position.y);
-                        mMass.Add((float)i.GetComponent<DynamicProperties>().size);
                         break;
                     case ForceType.Electron:
                         elecDistanceX.Add((float)i.transform.position.x);
                         elecDistanceY.Add((float)i.transform.position.y);
-                        mCharge.Add((float)i.GetComponent<DynamicProperties>().size);
                         break;
                     case ForceType.Fluxion:
                         fluxDistanceX.Add((float)i.transform.position.x);
                         fluxDistanceY.Add((float)i.transform.position.y);
-                        mFluxCapacity.Add((float)i.GetComponent<DynamicProperties>().size);
                         break;
                     default:
                         break;
@@ -93,25 +81,22 @@ public class Particle : MonoBehaviour
             
         }
 
-        elecForce = Electrostatic(elecDistanceX, elecDistanceY, mCharge, mProperties[1]);
-        fluxForce = Flux(fluxDistanceX, fluxDistanceY, mFluxCapacity, mProperties[2]);
+        elecForce = Electrostatic(elecDistanceX, elecDistanceY, mProperties[1]);
+        fluxForce = Flux(fluxDistanceX, fluxDistanceY, mProperties[2]);
 
-        resultant = Gravity(gravDistanceX, gravDistanceY, mMass, mProperties[0]) + elecForce + fluxForce;
+        resultant = Gravity(gravDistanceX, gravDistanceY, mProperties[0]) + elecForce + fluxForce;
 
         rb.AddForce(resultant, ForceMode2D.Impulse);
 
         gravDistanceX.Clear();
         gravDistanceY.Clear();
-        mMass.Clear();
         elecDistanceX.Clear();
         elecDistanceY.Clear();
-        mCharge.Clear();
         fluxDistanceX.Clear();
         fluxDistanceY.Clear();
-        mFluxCapacity.Clear();
     }
 
-    private Vector2 Gravity(List<float> xDistance, List<float> yDistance, List<float> mass, bool active) {
+    private Vector2 Gravity(List<float> xDistance, List<float> yDistance, bool active) {
         float totalXForce = 0;
         float totalYForce = 0;
         float force;
@@ -121,8 +106,7 @@ public class Particle : MonoBehaviour
             for (int i = 0; i < xDistance.Count; i++)
             {
                 Vector2 distance = new Vector2(currentX - xDistance[i], currentY - yDistance[i]);
-
-                force = (mass[i] * gravityConstant) / (Mathf.Pow(distance.magnitude, 2));
+                force = 200 / (Mathf.Pow(distance.magnitude, 2));
 
                 if (currentX - xDistance[i] > 0)
                 {
@@ -150,7 +134,7 @@ public class Particle : MonoBehaviour
         return gravForce;
     }
     
-    private Vector2 Electrostatic(List<float> xDistance, List<float> yDistance, List<float> charge, bool active)
+    private Vector2 Electrostatic(List<float> xDistance, List<float> yDistance, bool active)
     {
         float totalXForce = 0;
         float totalYForce = 0;
@@ -162,7 +146,7 @@ public class Particle : MonoBehaviour
             {
 
                 Vector2 distance = new Vector2(currentX - xDistance[i], currentY - yDistance[i]);
-                force = (charge[i] * gravityConstant) / (Mathf.Pow(distance.magnitude, 2));
+                force = 200 / Mathf.Pow(distance.magnitude, 2);
 
                 if (mProperties[3])
                 {
@@ -213,7 +197,7 @@ public class Particle : MonoBehaviour
         return elecForce;
     }
 
-    private Vector2 Flux(List<float> xDistance, List<float> yDistance, List<float> fluxcapacity, bool active)
+    private Vector2 Flux(List<float> xDistance, List<float> yDistance, bool active)
     {
         float totalXForce = 0;
         float totalYForce = 0;
@@ -225,7 +209,7 @@ public class Particle : MonoBehaviour
             {
 
                 Vector2 distance = new Vector2(currentX - xDistance[i], currentY - yDistance[i]);
-                force = (fluxcapacity[i] * fluxConstant) / (Mathf.Pow(distance.magnitude, 2));
+                force = 200 / Mathf.Pow(distance.magnitude, 2);
 
                 if (currentX - xDistance[i] > 0)
                 {
