ca60af1dd6756fc87a9b8d088773c12ab9b4ae6a
Zackamo <zackmj99@gmail.com>
2018-08-04 13:09:44 -0500

    end screen ui works

---
+++ Assets/Scripts/Editor Scripts/TrashCan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour {
+
+	Transform trash;
+
+	void OnTriggerEnter (Collider coll) {
+		Debug.Log ("Bonk");
+		Transform temp = coll.transform;
+		if (temp.GetComponent<DragAndDrop> ().isDragged) {
+			trash = temp;
+		}
+	}
+
+	void OnMouseUp () {
+		Debug.Log ("click");
+		Destroy (trash.gameObject);
+	}
+}
