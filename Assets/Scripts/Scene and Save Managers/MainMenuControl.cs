e658783f2a56a89f9c03accc1f67c9bf034212f4
Adrian <awkirchner@gmail.com>
2018-08-04 12:48:15 -0500

    next level button is done

--- Assets/Scripts/Scene and Save Managers/MainMenuControl.cs
+++ Assets/Scripts/Scene and Save Managers/MainMenuControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -26,9 +27,22 @@ public class MainMenuControl : MonoBehaviour {
             }
         }
 
-        if (level == "PreviousLevel")
-        {
+        if (level == "PreviousLevel") {
             level = GameProperties.previousLevel;
+        }
+        else if (level == "NextLevel") {
+            if (Int32.Parse(GameProperties.levelFilename.Substring(4, 2)) == 15) {
+                if (Int32.Parse(GameProperties.currentLevelPack) == GameProperties.saveData.GetLength(0) - 1) {
+                    level = "Level Select";
+                }
+                else {
+                    level = "Pack " + (Int32.Parse(GameProperties.currentLevelPack) + 1).ToString();
+                }
+            }
+            else {
+                GameProperties.levelFilename = GameProperties.levelFilename.Substring(0, 4) + (Int32.Parse(GameProperties.levelFilename.Substring(4, 2)) + 1).ToString().PadLeft(2, '0') + ".txt";
+                level = "Level";
+            }
         }
 
 		SceneManager.LoadScene (level);
