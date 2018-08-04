b1ba707f3a3ecbb120cac7a06c45bc4afc1dd056
Zackamo <zackmj99@gmail.com>
2018-08-04 18:01:46 -0500

    editor force bank works

---
+++ Assets/Scripts/Editor Scripts/EditorAvalible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorAvalible : MonoBehaviour {
+
+	public Transform force;
+	public bool isPositive;
+
+	public void click(){
+		if (isPositive) {
+			force.GetComponent<ForceSpawner> ().numAvailable++;
+		} 
+		else {
+			force.GetComponent<ForceSpawner> ().numAvailable--;
+		}
+	}
+}
