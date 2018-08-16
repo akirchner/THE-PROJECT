8fa3da6d9cca2732e101321042af6f9397d306c0
Zackamo <zackmj99@gmail.com>
2018-08-15 20:13:48 -0500

    trash can works

--- Assets/Scripts/Editor Scripts/TrashCan.cs
+++ Assets/Scripts/Editor Scripts/TrashCan.cs
@@ -6,16 +6,28 @@ public class TrashCan : MonoBehaviour {
 
 	Transform trash;
 
-	void OnTriggerEnter (Collider coll) {
+	void Update(){
+		if (Input.GetMouseButtonUp (0)) {
+			if(trash != null) {
+				if (trash.parent != null) {
+					Destroy (trash.parent.gameObject);
+				} 
+				else {
+					Destroy (trash.gameObject);
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit2D (){
+		trash = null;
+	}
+
+	void OnTriggerEnter2D (Collider2D coll) {
 		Debug.Log ("Bonk");
 		Transform temp = coll.transform;
 		if (temp.GetComponent<DragAndDrop> ().isDragged) {
 			trash = temp;
 		}
-	}
-
-	void OnMouseUp () {
-		Debug.Log ("click");
-		Destroy (trash.gameObject);
 	}
 }
