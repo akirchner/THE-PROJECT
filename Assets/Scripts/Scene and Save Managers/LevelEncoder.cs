892de496fd0be865b63d3c708bd4f7cc1d0e643d
OcarinaOfTardis <thetardis1968@gmail.com>
2018-09-19 18:05:54 -0500

    Level encoding working so far

---
+++ Assets/Scripts/Scene and Save Managers/LevelEncoder.cs
@@ -0,0 +1,471 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class LevelEncoder : MonoBehaviour
+{
+    private string filepath, code, line;
+    private int roundUp;
+
+    // Use this for initialization
+    void Start()
+    {
+        EncodeLevel("Grav", 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void EncodeLevel(string pack, int levelNumber1, int levelNumber2)
+    {
+        StringBuilder builder = new StringBuilder();
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            filepath = Application.persistentDataPath + "/" + pack + levelNumber1 + levelNumber2 + ".txt";
+
+            if (!File.Exists(filepath))
+            {
+                WWW load = new WWW("jar:file://" + Application.dataPath + "!/assets/" + pack + levelNumber1 + levelNumber2 + ".txt");
+                while (!load.isDone) { }
+
+                File.WriteAllBytes(filepath, load.bytes);
+            }
+        }
+        else
+        {
+            filepath = Path.Combine(Application.streamingAssetsPath, pack + levelNumber1 + levelNumber2 + ".txt");
+        }
+
+        StreamReader sr = new StreamReader(filepath);
+
+        line = sr.ReadLine();
+        List<string> levelData = new List<string>();
+
+        while (line != null)
+        {
+            levelData.Add(line);
+            line = sr.ReadLine();
+        }
+
+        sr.Close();
+
+        builder.Append(NumToLet((Int32.Parse(levelData[0]) % 26) + 1, Int32.Parse(levelData[0]) > 25));
+        builder.Append(NumToLet((Int32.Parse(levelData[1]) % 26) + 1, Int32.Parse(levelData[1]) > 25));
+        builder.Append(NumToLet((Int32.Parse(levelData[2]) % 26) + 1, Int32.Parse(levelData[2]) > 25));
+
+        for (int i = 3; i < levelData.Count; i++)
+        {
+            int tempID = Int32.Parse(levelData[i]);
+            float tempX = float.Parse(levelData[i + 1]);
+            float tempY = float.Parse(levelData[i + 2]);
+            float tempRot = float.Parse(levelData[i + 3]);
+            float xData1 = float.Parse(levelData[i + 4]);
+            float xData2 = float.Parse(levelData[i + 5]);
+            i += 5;
+
+            string id = "#";
+            string[] position = ParsePosition(tempX, tempY);
+            string[] rotation = ParseRotation(tempRot);
+            string[] scale = new string[2];
+
+            switch (tempID)
+            {
+                case 0: //Beam
+                    bool[] properties = new bool[4];
+
+                    for(int j = 0; j < properties.Length; j++)
+                    {
+                        properties[j] = xData1.ToString().ToCharArray()[j] == '1';
+                    }
+
+                    id = ParseBeam(properties);
+                    break;
+                case 1: //Goal
+                    id = "Q";
+                    scale = ParseScale(xData2, 1);
+                    break;
+                case 2: //Wall
+                    id = "P";
+                    scale = ParseScale(xData1, xData2);
+                    break;
+                case 3: //Dragable
+                    switch (xData1.ToString())
+                    {
+                        case "1":
+                            id = "D";
+                            break;
+                        case "2":
+                            id = "F";
+                            break;
+                        case "3":
+                            id = "E";
+                            break;
+                        default:
+                            Debug.Log("Invalid dragable force type! Type: " + xData1);
+                            break;
+
+                    }
+                    break;
+                case 4: //Dynamic
+                    string production = "#";
+                    string reaction = "#";
+
+                    switch(xData1.ToString())
+                    {
+                        case "1":
+                            production = "G";
+                            break;
+                        case "2":
+                            production = "F";
+                            break;
+                        case "3":
+                            production = "E";
+                            break;
+                        default:
+                            Debug.Log("Invalid dynamic production type! Type: " + xData1);
+                            break;
+                    }
+
+                    switch(xData2.ToString())
+                    {
+                        case "1":
+                            reaction = "G";
+                            break;
+                        case "2":
+                            reaction = "F";
+                            break;
+                        case "3":
+                            reaction = "P";
+                            break;
+                        case "4":
+                            reaction = "N";
+                            break;
+                        default:
+                            Debug.Log("Invalid dynamic reaction type! Type: " + xData2);
+                            break;
+                    }
+
+                    id = ParseDynamicForce(production, reaction);
+
+                    break;
+                case 5: //Static
+                    switch(xData1.ToString())
+                    {
+                        case "1":
+                            id = "A";
+                            break;
+                        case "2":
+                            id = "C";
+                            break;
+                        case "3":
+                            id = "B";
+                            break;
+                        default:
+                            Debug.Log("Invalid static force type! Type: " + xData1);
+                            break;
+
+                    }
+                    break;
+                case 6: //Mirror
+                    id = "O";
+                    scale = ParseScale(xData1, xData2);
+                    break;
+                case 7: //Wormhole
+                    id = NumToLet(xData1 + 18, true);
+                    break;
+                default:
+                    Debug.Log("Invalid ID in LevelEncoder! ID: " + tempID);
+                    break;
+            }
+
+            builder.Append(id);
+            builder.Append(position[0]);
+            builder.Append(position[1]);
+            builder.Append(position[2]);
+
+            switch(tempID) {
+                case 0: //Beam
+                    builder.Append(rotation[0]);
+                    builder.Append(rotation[1]);
+                    break;
+                case 1: //Goal
+                    builder.Append(rotation[0]);
+                    builder.Append(rotation[1]);
+                    builder.Append(scale[0]);
+                    break;
+                case 2: //Wall
+                case 6: //Mirror
+                    builder.Append(rotation[0]);
+                    builder.Append(rotation[1]);
+                    builder.Append(scale[0]);
+                    builder.Append(scale[1]);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        Debug.Log(builder.ToString());
+    }
+
+
+
+    private string ParseBeam(bool[] properties)
+    {
+        int value = 0;
+
+        if(properties[0])
+        {
+            value += 6;
+        }
+        if(properties[2])
+        {
+            value += 3;
+        }
+        if(properties[1])
+        {
+            if(properties[3])
+            {
+                value += 1;
+            }
+            else
+            {
+                value += 2;
+            }
+        }
+
+        string id = NumToLet(value, false);
+        return id;
+    }
+
+
+
+    private string ParseDynamicForce(string production, string reaction)
+    {
+        string id = "#";
+
+        switch (production + reaction) {
+            case "GF":
+                id = "G";
+                break;
+            case "GP":
+                id = "H";
+                break;
+            case "GN":
+                id = "I";
+                break;
+            case "EG":
+                id = "J";
+                break;
+            case "EF":
+                id = "K";
+                break;
+            case "FG":
+                id = "L";
+                break;
+            case "FP":
+                id = "M";
+                break;
+            case "FN":
+                id = "N";
+                break;
+            default:
+                Debug.Log("Invalid production/reaction! PR: " + production + reaction);
+                break;
+        }
+
+        return id;
+    }
+
+
+
+    private string[] ParsePosition(float x, float y)
+    {
+        string[] position = new string[3];
+        float xDec = ToNearestFifth(Math.Abs(x));
+        x += roundUp;
+        float yDec = ToNearestFifth(Math.Abs(y));
+        y += roundUp;
+
+        if(Math.Abs(x) <= 25)
+        {
+            position[0] = NumToLet((Math.Abs(x) - xDec) + 1, true);
+        }
+        else
+        {
+            position[0] = ((Math.Abs(x) - xDec) % 26).ToString();
+        }
+
+        position[1] = NumToLet((Math.Abs(y) - yDec) + 1, y > 0);
+
+        int decimalSearcher = 0;
+
+        while(xDec > 0.2)
+        {
+            decimalSearcher += 5;
+            xDec -= 0.2f;
+        }
+
+        while(yDec > 0.2)
+        {
+            decimalSearcher++;
+            yDec -= 0.2f;
+        }
+
+        position[2] = NumToLet(decimalSearcher + 1, x > 0);
+
+        return position;
+    }
+
+    
+
+    private string[] ParseRotation(float degreeRot)
+    {
+        string[] rotation = new string[2];
+        degreeRot %= 360;
+
+        if(degreeRot < 0)
+        {
+            degreeRot = 360 + degreeRot;
+        }
+
+        float degDec = ToNearestQuarter(degreeRot % 1);
+        degreeRot += roundUp;
+
+        rotation[0] = NumToLet((degreeRot / 26) + 1, (degDec == 0.5 || degDec == 0.75));
+        rotation[1] = NumToLet((degreeRot % 26) + 1, (degDec == 0.25 || degDec == 0.75));
+
+        return rotation;
+    }
+
+    
+
+    private string[] ParseScale(float xScl, float yScl)
+    {
+        string[] scale = new string[2];
+        xScl = Math.Abs(xScl);
+        yScl = Math.Abs(yScl);
+
+        float xDec = ToNearestHalf(xScl % 1);
+        xScl += roundUp;
+        float yDec = ToNearestHalf(yScl % 1);
+        yScl += roundUp;
+
+        scale[0] = NumToLet((xScl % 26) + 1, xDec == 0.5);
+        scale[1] = NumToLet((yScl % 26) + 1, yDec == 0.5);
+
+        return scale;
+    }
+
+
+
+    private string NumToLet(float number, bool isCaps)
+    {
+        char c = (char)((isCaps ? 65 : 97) + (int) (number - 1));
+        return c.ToString();
+    }
+
+
+
+    private float ToNearestFifth(float number)
+    {
+        number %= 1;
+
+        if (number >= 0.9)
+        {
+            roundUp = 1;
+        }
+        else
+        {
+            roundUp = 0;
+        }
+
+        if (number >= 0.9 || number < 0.1)
+        {
+            number = 0f;
+        }
+        else if(number >= 0.1 && number < 0.3)
+        {
+            number = 0.2f;
+        }
+        else if(number >= 0.3 && number < 0.5)
+        {
+            number = 0.4f;
+        }
+        else if(number >= 0.5 && number < 0.7)
+        {
+            number = 0.6f;
+        }
+        else
+        {
+            number = 0.8f;
+        }
+
+        return number;
+    }
+
+    private float ToNearestQuarter(float number)
+    {
+        number %= 1;
+
+        if(number >= 0.875)
+        {
+            roundUp = 1;
+        }
+        else
+        {
+            roundUp = 0;
+        }
+
+        if(number >= 0.875 || number < 0.125)
+        {
+            number = 0f;
+        }
+        else if(number >= 0.125 && number < 0.375)
+        {
+            number = 0.25f;
+        }
+        else if(number >= 0.375 && number < 0.625)
+        {
+            number = 0.5f;
+        }
+        else
+        {
+            number = 0.75f;
+        }
+
+        return number;
+    }
+
+    private float ToNearestHalf(float number)
+    {
+        number %= 1;
+
+        if (number >= 0.75)
+        {
+            roundUp = 1;
+        }
+        else
+        {
+            roundUp = 0;
+        }
+
+        if (number >= 0.75 || number < 0.25)
+        {
+            number = 0f;
+        }
+        else
+        {
+            number = 0.5f;
+        }
+
+        return number;
+    }
+}
