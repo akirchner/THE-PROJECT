ed94b7e6e30ff3956cce7e58e34b8c8772dfeaa6
Zackamo <zackmj99@gmail.com>
2018-11-12 21:20:27 -0600

    commented main menu control

--- Assets/Scripts/Scene and Save Managers/MainMenuControl.cs
+++ Assets/Scripts/Scene and Save Managers/MainMenuControl.cs
@@ -7,7 +7,9 @@ using UnityEngine.SceneManagement;
 public class MainMenuControl : MonoBehaviour {
 
 	public void LoadScene (string level){
+		//loads the given scene, except for several exeptions:
 
+		//handles the back button in scene: Editor Save, which leads to different scenes depending on where you came from
 		if(level == "Editor/Share Level"){// this checks the old 'previous level' and thus does need to go here
 			if (GameProperties.previousLevel == "Editor") {
 				level = "Editor";
@@ -16,11 +18,14 @@ public class MainMenuControl : MonoBehaviour {
 				level = "Share Level";
 			}
 		}
+
 		GameProperties.previousLevel = GameProperties.currentLevel;
 
+		//saves the current pack number for future reference
         if (level.Substring(0, 4).ToUpper() == "PACK") {
             GameProperties.currentLevelPack = level.Substring(5, 1);
         }
+		//loads the current pack based on above
         else if (level == "CurrentPack") {
             if (GameProperties.currentLevelPack == "") {
                 level = "Level Select";
@@ -29,9 +34,11 @@ public class MainMenuControl : MonoBehaviour {
                 level = "Pack " + GameProperties.currentLevelPack;
             }
         }
+		//handles the replay button
         else if (level == "PreviousLevel") {
             level = GameProperties.previousLevel;
         }
+		//handles the next level button
         else if (level == "NextLevel") {
             if (Int32.Parse(GameProperties.levelFilename.Substring(4, 2)) == 15) {
                 if (Int32.Parse(GameProperties.currentLevelPack) == GameProperties.saveData.GetLength(0) - 1) {
@@ -46,14 +53,17 @@ public class MainMenuControl : MonoBehaviour {
                 level = "Level";
             }
         }
+		//sets the level file to the editor temp file when the editor is loaded
         else if (level =="Editor") {
             GameProperties.levelFilename = "Edit00.txt";
         }
+		//saves the editor temp file and loads it into editor level
         else if (level == "Editor Level") {
             GameObject.Find("Level Controller").GetComponent<SaveLevel>().save("Edit00.txt");
             GameProperties.levelFilename = "Edit00.txt";
         }
 
+		//main case of LoadScene
 		SceneManager.LoadScene(level);
         GameProperties.currentLevel = level;
 
