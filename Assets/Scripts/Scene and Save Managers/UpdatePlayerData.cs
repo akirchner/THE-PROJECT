1da6537c3cd2d8af8cd651c23dd5d5813e9acebf
OcarinaOfTardis <thetardis1968@gmail.com>
2018-12-19 11:56:05 -0600

    Visual indicatior for whether or not a player level exists

---
+++ Assets/Scripts/Scene and Save Managers/UpdatePlayerData.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatePlayerData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        try
+        {
+            string filepath = Path.Combine(Application.streamingAssetsPath, "playerData.txt");
+
+            if (File.Exists(filepath))
+            {
+                StreamReader sr = new StreamReader(filepath);
+
+                for(int i = 0; i < 15; i++)
+                {
+                    GameProperties.playerLevelData[i] = System.Int32.Parse(sr.ReadLine()) == 1;
+                }
+
+                sr.Close();
+            }
+        }
+        catch (System.NullReferenceException)
+        {
+            Debug.Log("No player levels save file found!");
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        for(int i = 0; i < 15; i++)
+        {
+            if(GameProperties.playerLevelData[i])
+            {
+                GameObject.Find("Level Button " + (i + 1)).GetComponent<Image>().color = Color.white;
+            }
+            else
+            {
+                GameObject.Find("Level Button " + (i + 1)).GetComponent<Image>().color = Color.grey;
+            }
+        }
+    }
+
+    public void newLevel(int levelNum)
+    {
+        GameProperties.playerLevelData[levelNum - 1] = true;
+    }
+
+    public void save()
+    {
+        StreamWriter sw = File.CreateText(Path.Combine(Application.streamingAssetsPath, "playerData.txt"));
+
+        for (int i = 0; i < 15; i++)
+        {
+            if(GameProperties.playerLevelData[i])
+            {
+                sw.WriteLine("1");
+            }
+            else
+            {
+                sw.WriteLine("0");
+            }
+        }
+
+        sw.Close();
+    }
+}
