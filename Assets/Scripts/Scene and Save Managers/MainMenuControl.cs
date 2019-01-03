69505c67413317744c5298847ba4b420da1ae2cd
Zackamo <zackmj99@gmail.com>
2019-01-03 11:54:37 -0600

    fixed flux 15 next level button

--- Assets/Scripts/Scene and Save Managers/MainMenuControl.cs
+++ Assets/Scripts/Scene and Save Managers/MainMenuControl.cs
@@ -49,7 +49,7 @@ public class MainMenuControl : MonoBehaviour {
 		//handles the next level button
         else if (level == "NextLevel") {
             if (Int32.Parse(GameProperties.levelFilename.Substring(4, 2)) == 15) {
-                if (Int32.Parse(GameProperties.currentLevelPack) == GameProperties.saveData.GetLength(0) - 1) {
+				if (Int32.Parse(GameProperties.currentLevelPack) == 3) {//three is the index of the last pack (flux), increment if packs are added 
                     level = "Level Select";
                 }
                 else {
